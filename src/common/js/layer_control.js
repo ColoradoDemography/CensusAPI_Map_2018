6d3e293b534d4533e21c35faa0a00191d1666d2f
Todd Bleess <todd.bleess@gmail.com>
2019-03-20 19:12:07 +0000

    Toggle city boundaries

--- src/common/js/layer_control.js
+++ src/common/js/layer_control.js
@@ -5,8 +5,27 @@ module.exports = function(cMap) {
         'clickable': 'false',
         'zIndex': 1000
     });
-
-
+    
+    // var munibounds = L.mapbox.feature('cjthelhtl01fg32pd2gcjfuc9', {
+    //     'clickable': 'false'
+    // });
+    
+    var munibounds = new L.geoJson();
+    
+    
+    $.ajax({
+        dataType: "json",
+        url: "https://gis.dola.colorado.gov/munis/munis",
+        success: function(data) {
+            $(data.features).each(function(key, data) {
+                munibounds.addData(data);
+            });
+            munibounds.setStyle({color: '#000000', opacity: 1.0, fillColor: '#ffffff', fillOpacity: 0.1});
+        }
+        }).error(function() {});
+        
+    
+    
     //create map sandwich
     var topPane = cMap.map._createPane('leaflet-top-pane', cMap.map.getPanes().mapPane);
     var topLayer = mblabels.addTo(cMap.map);
@@ -20,7 +39,7 @@ module.exports = function(cMap) {
 
     //in the future ill figure out how to toggle labels on and off (and still have it appear on top)
     var groupedOverlays = {
-        //  "Labels and Borders": mblabels
+        "Municipal Boundaries": munibounds
     };
 
     //add layer control
